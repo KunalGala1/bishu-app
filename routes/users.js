3a4733da14b54bc6cc0a6255da4f749dd6108552
Kunal Gala <kunal.gala16@gmail.com>
2024-03-07 10:09:19 -0500

    Removed copyright from other pages besides Home Page, removed alt text from Home Page About preview image, reorganized quotes, added video to About Page, added NameSplash to all pages, added User

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-const { forwardAuthenticated } = require('../config/auth');
-const { ensureAuthenticated } = require('../config/auth');
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const { forwardAuthenticated } = require("../config/auth");
+const { ensureAuthenticated } = require("../config/auth");
 
-router.get('/login', forwardAuthenticated, async (req, res) => {
-  res.render('client/users/login');
+router.get("/login", forwardAuthenticated, async (req, res) => {
+  res.render("client/users/login");
 });
 
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login?error=true',
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login?error=true",
   })(req, res, next);
 });
 
-router.get('/logout', function (req, res, next) {
+router.get("/logout", function (req, res, next) {
   req.logout(function (err) {
     if (err) {
       return next(err);
     }
-    res.redirect('/login');
+    res.redirect("/login");
   });
 });
 
